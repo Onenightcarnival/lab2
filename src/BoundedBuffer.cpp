64f1f98d5405637ce7a6db7874bf7844b73c331f
Onenightcarnival <244183238li@gmail.com>
2017-10-30 11:40:23 -0400

    2017.10.30

--- src/BoundedBuffer.cpp
+++ src/BoundedBuffer.cpp
@@ -1,20 +1,59 @@
 #include "BoundedBuffer.h"
+#include <pthread.h>
 
 BoundedBuffer::BoundedBuffer(int N){
 	//TODO: constructor to initiliaze all the varibales declared in BoundedBuffer.h
+	buffer = new int[N]();
+	buffer_size = N;
+	buffer_cnt = 0;
+	buffer_last = N - 1;
+	pthread_cond_init(&buffer_full, NULL);
+	pthread_cond_init(&buffer_empty, NULL);
+	pthread_mutex_init(&buffer_lock, NULL);
 }
 
 
 void BoundedBuffer::append(int data){
 	//TODO: append a data item to the circular buffer
+	pthread_mutex_lock(&buffer_lock);
+	while(buffer_cnt == buffer_size){
+		pthread_cond_wait(&buffer_full, &buffer_lock);
+	}
+	buffer_last = (buffer_last + 1) % buffer_size;
+	buffer[buffer_last] = data;
+	buffer_cnt ++;
+	pthread_cond_signal(&buffer_empty);
+	pthread_mutex_unlock(&buffer_lock);
 }
 
 int BoundedBuffer::remove(){
 	//TODO: remove and return a data item from the circular buffer
-	return -1;
+	pthread_mutex_lock(&buffer_lock);
+	while(buffer_cnt == 0){
+		pthread_cond_wait(&buffer_empty, &buffer_lock);
+	}
+	if((buffer_last - (buffer_cnt - 1)) % buffer_size < 0){
+		int removed = buffer[(buffer_last - (buffer_cnt - 1)) % buffer_size + buffer_size];
+		buffer_cnt --;
+		pthread_cond_signal(&buffer_full);
+		pthread_mutex_unlock(&buffer_lock);
+		return removed;
+	}
+	else{
+		int removed = buffer[(buffer_last - (buffer_cnt - 1)) % buffer_size];
+		buffer_cnt --;
+		pthread_cond_signal(&buffer_full);
+		pthread_mutex_unlock(&buffer_lock);
+		return removed;
+	}
 }
 
 bool BoundedBuffer::isEmpty(){
 	//TODO: check is the buffer is empty
-	return true;
+	if(buffer_cnt == 0){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
--- src/ProducerConsumer.cpp
+++ src/ProducerConsumer.cpp
@@ -1,16 +1,103 @@
 #include "ProducerConsumer.h"
-
+#include <thread>
+#include <sys/time.h>
+#include <chrono>
+#include <atomic>
+using namespace std;
 //TODO: add BoundedBuffer, locks and any global variables here
+
+atomic<int> sharedP(0);
+atomic<int> sharedC(0);
+BoundedBuffer *buff = new BoundedBuffer (10);
+int pSleep;
+int cSleep;
+int item_cnt;
+ofstream output;
+pthread_mutex_t lock;
+double tBegin = 0;
+atomic<int> buffer_cnt(0);
 
 void InitProducerConsumer(int p, int c, int psleep, int csleep, int items){
 	//TODO: constructor to initialize variables declared
 	//also see instruction for implementation
-}
+	pthread_mutex_init(&lock, NULL);
+	pthread_t producers[p];
+	pthread_t consumers[c];
+	cSleep = csleep;
+	pSleep = psleep;
+	item_cnt = items;
+	output.open("output.txt");
 
+	struct timeval t;
+	gettimeofday(&t, NULL);
+	tBegin = (double)t.tv_sec * 1000 + (double)t.tv_usec / 1000;
+	
+	for(int i = 0; i < p; i++){
+		pthread_create(&producers[i], NULL, producer, (void *)(intptr_t) i);
+	}
+
+	for(int i = 0; i < c; i++){
+		pthread_create(&consumers[i], NULL, consumer, (void *)(intptr_t) i);
+	}
+
+	for(int i = 0; i < p; i++){
+		pthread_join(producers[i], NULL);
+	}
+
+	for(int i = 0; i < c; i++){
+		pthread_join(consumers[i], NULL);
+	}
+
+	output.close();
+}
+// Producer #i, time = current time, producing data item #j, item value=foo
+// Consumer #k, time = current time, consuming data item with value=foo
 void* producer(void* threadID){
 	//TODO: producer thread, see instruction for implementation
+	int id = (intptr_t)threadID;
+	while(1){
+		this_thread::sleep_for(chrono::milliseconds(pSleep));
+		if(sharedP < item_cnt){
+			int add = rand() % 10;
+			sharedP.fetch_add(1);
+			buff->append(add);
+			buffer_cnt.fetch_add(1);
+			struct timeval t;
+			gettimeofday(&t, NULL);
+			double currentTime = (double)t.tv_sec * 1000 + (double)t.tv_usec / 1000;
+			pthread_mutex_lock(&lock);
+			output << "Producer #" << id << ", time = " 
+			<< currentTime - tBegin << ", producing data item #" 
+			<< sharedP << ", item value = " << add << endl;
+			pthread_mutex_unlock(&lock);
+		}
+		else{
+			break;
+		}
+	}
+	return NULL;
 }
 
 void* consumer(void* threadID){
 	//TODO: consumer thread, see instruction for implementation
+	int id = (intptr_t)threadID;
+	while(1){
+		this_thread::sleep_for(chrono::milliseconds(cSleep));
+		if(sharedC < buffer_cnt){
+			sharedC.fetch_add(1);
+			int removed = buff->remove();
+			struct timeval t;
+			gettimeofday(&t, NULL);
+			double currentTime = (double)t.tv_sec * 1000 + (double)t.tv_usec / 1000;
+			pthread_mutex_lock(&lock);
+			output << "Consumer #" << id << ", time = " 
+			<< currentTime - tBegin << ", consuming data item with value = " 
+			<< removed << endl;
+			pthread_mutex_unlock(&lock);
+		}
+		else if(sharedC >= item_cnt){
+			break;
+		}
+	}
+	return NULL;
 }
